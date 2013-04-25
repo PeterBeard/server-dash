7234cb308326cba0dfcb3126d41c872803340e69
Peter Beard <peter.b.beard@gmail.com>
2013-04-25 14:01:47 -0400

    Number of packages to update reported as 0 instead of ''

--- json/packages.php
+++ json/packages.php
@@ -3,9 +3,9 @@ exec('dpkg --list | wc -l', $installed);
 $installed = number_format($installed[0],0);
 $package_updates = file_get_contents('/var/lib/update-notifier/updates-available');
 preg_match('/(\d+) packages.+/',$package_updates,$matches);
-$updates = $matches[1];
+$updates = empty($matches[1]) ? '0' : $matches[1];
 preg_match('/(\d+) updates.+/',$package_updates,$matches);
-$security = $matches[1];
+$security = empty($matches[1]) ? '0' : $matches[1];
 ?>
 {
 	"installed":"<?php echo $installed; ?>",
